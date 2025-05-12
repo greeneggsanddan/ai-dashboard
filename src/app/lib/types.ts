f22a2cbd83c6d7d862b7deaa1fb292a4efdc1dee
greeneggsanddan <greeneggsanddan@gmail.com>
2025-05-12 16:17:31 -0700

    Add Owner interface

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -2,16 +2,23 @@ export interface Repo {
   id: number;
   name: string;
   full_name: string;
+  owner: Owner;
   description: string | null;
   html_url: string;
   stargazers_count: number;
   language: string;
-  owner: string;
   topics: string[];
   created_at: string;
   updated_at: string;
   forks_count: number;
   open_issues_count: number;
+}
+
+interface Owner {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
 }
 
 export type TimeRange = 'all-time' | 'year' | 'month';
